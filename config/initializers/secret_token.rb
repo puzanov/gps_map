7c5d9d23c046d9062fcb6ee3a0bca7cf7b32d422
puzanov <puzanov@gmail.com>
2011-07-27 17:10:39 +0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GpsMap::Application.config.secret_token = 'df1440686f164faab07d9d5d1b118197bd788e31a60c27030b5f805c04fb7449bb9158bff5d58796f6bdb7974f20e5322ee258761c5bb3ac687b93cf323752de'
