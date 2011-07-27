81bec6b2e09b78dc1ccf5d27e7fe1d17fb0a11f5
puzanov <puzanov@gmail.com>
2011-07-27 18:39:29 +0600

    markers added and icons added to the map

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+  def index
+    #@map = GMap.new("map_div")
+    #@map.control_init(:large_map => true,:map_type => true)
+    #@map.center_zoom_init([42.878408,74.618534],4)
+  end
+end
