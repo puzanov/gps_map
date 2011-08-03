7c1fae519e1fa1dffae16538a134c7ec3fa062ac
puzanov <puzanov@gmail.com>
2011-08-03 15:39:07 +0600

    gmap keys helper added

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'http://rubygems.org'
 gem 'rails', '3.0.9'
 gem 'sqlite3'
+gem 'thin'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def get_gmap_key
+    gmap_keys = Hash.new
+    gmap_keys['192.168.8.119'] = "ABQIAAAAgzWLEmj3ztT5D6kx9AR-VxStd8WZxRdEel0fl4LO3dgc76hJThT79K0DWCr_TxhO3QyDTy31jjDFkQ"
+    gmap_keys['31.192.250.29'] = "ABQIAAAAgzWLEmj3ztT5D6kx9AR-VxQ9BVX71IRodHhOX26UUoBFCK7MDRRXLMl1YnYXkENaDVQG7nou6tGCsQ"
+    gmap_keys[request.host]
+  end
 end
